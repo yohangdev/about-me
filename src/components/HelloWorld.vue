484a2e4d6dcffc1906ac82164cd8ee9dfe58ce76
Yoga Hanggara <yohang88@gmail.com>
2019-04-06 15:13:23 +0700

    add social media links
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
     <div class="bg-blue-darker">
       <div class="container mx-auto px-4">
         <div
-          class="relative bg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden"
+          class="relative bg-white mx-auto max-w-sm shadow-md rounded-lg overflow-hidden"
           style="top: 50px"
         >
           <div class="sm:flex sm:items-center px-6 py-4">
@@ -13,16 +13,17 @@
               alt
             >
             <div class="text-center sm:text-left sm:flex-grow">
-              <div class="mb-4">
+              <div class="mb-5">
                 <p class="text-xl leading-tight">Yoga Hanggara</p>
                 <p
                   class="text-sm leading-tight text-grey-dark"
                 >Backend Engineer at Jabar Digital Service</p>
               </div>
-              <div>
-                <button
-                  class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-teal text-teal hover:bg-teal hover:text-white"
-                >Message</button>
+              
+              <div class="mb-2">
+                <a href="#" class="bg-grey-light hover:bg-grey py-2 px-4 text-grey-darker text-sm no-underline rounded-full">
+                  <i class="fas fa-envelope"></i> Contact me
+                </a>
               </div>
             </div>
           </div>
@@ -30,10 +31,53 @@
       </div>
     </div>
 
-    <div class="container mx-auto mt-24 px-4">
-      <div class="flex -mx-2">
-        <div class="w-1/2 h-12 px-2">
-          <div class="max-w rounded overflow-hidden shadow-lg">
+    <div class="container mx-auto mt-24 px-4 mb-4">
+      
+      <div class="mx-auto max-w-sm mb-8">
+        <div class="flex">
+
+          <div class="flex-1 text-center">
+            <a href="https://facebook.com/yohang" target="_blank" class="no-underline text-grey-darker text-5xl">
+              <i class="fab fa-linkedin"></i>
+            </a>
+            <div class="text-grey-darker text-sm mt-4">
+              LinkedIn
+            </div>
+          </div>
+
+          <div class="flex-1 text-center">
+            <a href="https://medium.com/@yohang88" target="_blank" class="no-underline text-grey-darker text-5xl">
+              <i class="fab fa-medium"></i>
+            </a>
+            <div class="text-grey-darker text-sm mt-4">
+              Medium
+            </div>
+          </div>
+
+          <div class="flex-1 text-center">
+            <a href="https://github.com/yohang88" target="_blank" class="no-underline text-grey-darker text-5xl">
+              <i class="fab fa-github"></i>
+            </a>
+            <div class="text-grey-darker text-sm mt-4">
+              Github
+            </div>
+          </div>
+
+          <div class="flex-1 text-center">
+            <a href="https://facebook.com/yohang" target="_blank" class="no-underline text-grey-darker text-5xl">
+              <i class="fab fa-facebook"></i>
+            </a>
+            <div class="text-grey-darker text-sm mt-4">
+              Facebook
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex flex-wrap -mx-2">
+        
+        <div class="w-full sm:w-1/2 lg:w-1/4 px-2 mb-6">
+          <div class="max-w rounded overflow-hidden shadow-md">
             <div class="px-6 py-4">
               <div class="mb-2">The Coldest Sunset</div>
               <p
@@ -43,8 +87,8 @@
           </div>
         </div>
 
-        <div class="w-1/2 h-12 px-2">
-          <div class="max-w rounded overflow-hidden shadow-lg">
+        <div class="w-full sm:w-1/2 lg:w-1/4 px-2 mb-6">
+          <div class="max-w rounded overflow-hidden shadow-md">
             <div class="px-6 py-4">
               <div class="mb-2">The Coldest Sunset</div>
               <p
@@ -53,6 +97,29 @@
             </div>
           </div>
         </div>
+
+        <div class="w-full sm:w-1/2 lg:w-1/4 px-2 mb-6">
+          <div class="max-w rounded overflow-hidden shadow-md">
+            <div class="px-6 py-4">
+              <div class="mb-2">The Coldest Sunset</div>
+              <p
+                class="text-grey-darker text-base"
+              >Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="w-full sm:w-1/2 lg:w-1/4 px-2 mb-6">
+          <div class="max-w rounded overflow-hidden shadow-md">
+            <div class="px-6 py-4">
+              <div class="mb-2">The Coldest Sunset</div>
+              <p
+                class="text-grey-darker text-base"
+              >Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   </div>
